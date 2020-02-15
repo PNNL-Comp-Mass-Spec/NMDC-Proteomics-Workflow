3574ec2db2ad304407abee20e1b1031c6c261048
Anubhav, Fnu <anubhav@pnnl.gov>
2020-02-14 16:29:02 -0800

    NMDC-6: download relevant analysis job files from DMS.

--- src/MetProWorkflowApp.py
+++ src/MetProWorkflowApp.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-'''Automate the proteomics workflow'''
-
 from src.data_access.via_DMS.Input import Input
 from src.data_access.via_DMS.QueryBuilder import QueryBuilder
 from src.processing.FileOperations import FileOperations
 
 class MetProWorkflowApp:
+    '''Automates the Meta-proteomics workflow'''
     def __init__(self):
         pass
 
@@ -18,12 +16,14 @@ class MetProWorkflowApp:
         # build & execute query
         myQuery= QueryBuilder(user_obj)
         myQuery.execute()
-        all_info, job_info=myQuery.analysis_jobs, myQuery.job_info
-        # print(all_info.shape, all_info.columns.values,'\n' ,job_info.shape, job_info.columns.values)
+        # pickup the objects
+        analysis_jobs, job_info, parent_data_folder= myQuery.analysis_jobs, myQuery.job_info, myQuery.parent_data_folder
+
+        # Download relevant files from DMS
+        file_obj= FileOperations(analysis_jobs, parent_data_folder)
+        file_obj.get_files()
 
         # Start merge Process
-        file_obj= FileOperations(all_info)
-        file_obj.get_files()
 
         # print(crossTab.shape, all_info.columns.values)
 
@@ -32,5 +32,4 @@ class MetProWorkflowApp:
 
 
 if __name__ == '__main__':
-
     MetProWorkflowApp().start_workflow()
